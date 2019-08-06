9dcbebfd1657bbd0b5260aba51c5e40199f72735
Tyson Hofstetter <tysonh652@gmail.com>
2019-08-06 15:48:19 +1000

    init commit

---
+++ site/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/tysonhofstetter/Sites/Gatsby/gatsby-themes/node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[],"defaultLayouts":{"default":"/Users/tysonhofstetter/Sites/Gatsby/gatsby-themes/packages/theme/src/components/layout.js"}},
+    }]
